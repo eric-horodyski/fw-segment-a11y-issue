b16a5fb44a5756aadf982ea99207f9483dfd6882
Eric Horodyski <horodyski@ionic.io>
2021-03-16 13:42:17 -0400

    Reproduce the issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSegment,
+  IonSegmentButton,
+} from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
+  const [selectedSegment, setSelectedSegment] = useState<string>("customers");
+
+  const handleSegmentSelect = (segment: string) => {
+    setSelectedSegment(segment);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +30,15 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonSegment
+          onIonChange={(e) => {
+            handleSegmentSelect(e.detail.value!);
+          }}
+          value={selectedSegment}
+        >
+          <IonSegmentButton value="customers">Customers</IonSegmentButton>
+          <IonSegmentButton value="employees">Employees</IonSegmentButton>
+        </IonSegment>
       </IonContent>
     </IonPage>
   );
